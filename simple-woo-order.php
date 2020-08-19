3183eea5161b7def935f40e480100af9b539244c
Ahmed Maruf <maruf@cefalo.com>
2020-08-19 15:21:34 +0600

    Implementing coupon or discount

--- simple-woo-order.php
+++ simple-woo-order.php
@@ -71,7 +71,7 @@ function swo_admin_page()
         </div>
         <div class='qofw-form-container'>
             <div class="qofw-form">
-                <form action='' class='pure-form pure-form-aligned' method='POST'>
+                <form action='<?php echo esc_url(admin_url('admin-post.php')); ?>' class='pure-form pure-form-aligned' method='POST'>
                     <fieldset>
                         <input type='hidden' name='customer_id' id='customer_id' value='0'>
                         <div class='pure-control-group'>
@@ -131,16 +131,18 @@ function swo_admin_page()
                             <select class='qofw-control' name='item' id='item'>
                                 <option value="0"><?php _e('Select One', 'swo'); ?></option>
                                 <?php
-                                $products = wc_get_products(array('post_status' => 'published', 'posts_per_page' => -1));
+                                $products = wc_get_products(array(
+                                    'post_status' => 'published', 'posts_per_page' => -1
+                                ));
 
-                                foreach ($products as $product){
-                                ?>
-                                        <option value="<?php echo $product->get_ID();?>">
-                                            <?php echo $product->get_Name()?>
-                                        </option>
+                                foreach ($products as $product) {
+                                    ?>
+                                    <option value="<?php echo $product->get_ID(); ?>">
+                                        <?php echo $product->get_Name() ?>
+                                    </option>
                                     <?php
                                 }
-                                    ?>
+                                ?>
                             </select>
                         </div>
 
@@ -171,6 +173,81 @@ function swo_admin_page()
     <?php
 }
 
+add_action('admin_post_swo_form', function () {
+    if (isset($_POST['submit'])) {
+        $order_id = swo_process_order();
+        wp_safe_redirect(
+                esc_url_raw(add_query_arg('order_id', $order_id, admin_url('admin.php?page=quick-order-create')))
+        );
+    }
+});
+
+function swo_process_order()
+{
+    //For new user
+    if (sanitize_text_field($_POST['customer_id']) == 0) {
+        $email = strtolower(sanitize_text_field($_POST['email']));
+        $first_name = sanitize_text_field($_POST['first_name']);
+        $last_name = sanitize_text_field($_POST['last_name']);
+        $password = sanitize_text_field($_POST['password']);
+        $phone_number = sanitize_text_field($_POST['phone']);
+        $customer_id = wp_create_user($email, $password, $email);
+        update_user_meta($customer_id, 'first_name', $first_name);
+        update_user_meta($customer_id, 'last_name', $last_name);
+        update_user_meta($customer_id, 'phone_number', $phone_number);
+        $customer = new WP_User($customer_id);
+
+    } else {
+        $customer = new WP_User(sanitize_text_field($_POST['customer_id']));
+    }
+    //Woocommerce integration
+    WC()->frontend_includes();
+    WC()->session = new WC_Session_Handler();
+    WC()->customer = new WC_Customer($customer->ID, 1);
+
+    $cart = new WC_Cart();
+    WC()->cart = $cart;
+    $cart->empty_cart();
+    $cart->add_to_cart(sanitize_text_field($_POST['item']), 1);
+
+    $discount = trim(sanitize_text_field($_POST['discount']));
+    if ($discount == ''){
+        $discount = 0;
+    }
+
+    $is_coupon = isset($_POST['coupon']);
+
+
+    $checkout = WC()->checkout();
+    $phone = sanitize_text_field($_POST['phone']);
+    $order_id = $checkout->create_order(array(
+        'billing_phone' => $phone,
+        'billing_email' => $customer->user_email,
+        'payment_method' => 'cash',
+        'billing_first_name' => $customer->first_name,
+        'billing_last_name' => $customer->last_name,
+    ));
+
+    $order = wc_get_order($order_id);
+    update_user_meta($order_id, '_customer_user', $customer->ID);
+
+    if ($is_coupon){
+        $order->apply_coupon($discount);
+    } elseif ($discount) {
+        $total = $order->calculate_totals();
+        $order->set_discount_total($discount);
+        $order->set_total($total - floatval($discount));
+    }
+    if (isset($_POST['note']) && !empty($_POST['note'])){
+        $order_note = apply_filters('swo_order_note', sanitize_text_field($_POST['note']), $order_id);
+        $order->add_order_note($order_note);
+    }
+
+    $order_status = apply_filters('swo_order_status', 'processing');
+    $order->set_status($order_status);
+    do_action('swo_order_complete', $order_id);
+    return $order->save();
+}
 
 add_action('wp_ajax_swo_fetch_user', function () {
     $email = strtolower(sanitize_text_field($_POST['email']));
@@ -196,6 +273,6 @@ add_action('wp_ajax_swo_fetch_user', function () {
     }
 });
 
-add_action('wp_ajax_swo_gen_password', function (){
+add_action('wp_ajax_swo_gen_password', function () {
     wp_send_json(wp_generate_password(12), 200);
 });
